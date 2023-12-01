3dfcef4d23548c244582db37724546888b31373e
PAMPILLON PARRA <O008839@ad.bbva.com>
2023-12-01 09:37:37 +0100

    first commit

---
+++ src/main/java/com/hackathon/equipo2/CineHackacthon/CineHackacthonApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.equipo2.CineHackacthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineHackacthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineHackacthonApplication.class, args);
+	}
+
+}
