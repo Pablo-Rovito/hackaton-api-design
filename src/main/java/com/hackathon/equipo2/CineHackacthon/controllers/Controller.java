a9ccd575dfaafef9d329402aa4bfeaa482107fa4
vicfuen02 <vicfuen02@gmail.com>
2023-12-01 13:19:42 +0100

    shows - add showController

---
+++ src/main/java/com/hackathon/equipo2/CineHackacthon/controllers/Controller.java
@@ -0,0 +1,11 @@
+package com.hackathon.equipo2.CineHackacthon.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/theater/v1/")
+public class Controller {
+
+}
