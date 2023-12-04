da36cf9cd65374cca80acb8666f7944197489723
= <=>
2023-12-04 12:01:21 +0100

    feature/abstract-room-validator
    feature: add implementation of AbstractValidator

--- src/main/java/com/hackathon/equipo2/CineHackacthon/services/RoomService.java
+++ src/main/java/com/hackathon/equipo2/CineHackacthon/services/RoomService.java
@@ -1,14 +1,17 @@
 package com.hackathon.equipo2.CineHackacthon.services;
 
 import com.hackathon.equipo2.CineHackacthon.models.RoomModel;
+import com.hackathon.equipo2.CineHackacthon.models.ValidatorModel;
 import com.hackathon.equipo2.CineHackacthon.repositories.RoomRepository;
 import com.hackathon.equipo2.CineHackacthon.services.responses.RoomServiceResponse;
 import com.hackathon.equipo2.CineHackacthon.utils.RoomEnum;
-import com.hackathon.equipo2.CineHackacthon.validators.RoomValidator;
+import com.hackathon.equipo2.CineHackacthon.validators.roomValidators.RoomValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
@@ -23,51 +26,92 @@ public class RoomService {
 
     public RoomServiceResponse<RoomModel> findById(long roomId) {
 
-        return roomValidator
-                .exists(roomRepository.findById(roomId));
+        Optional<RoomModel> room = roomRepository.findById(roomId);
+
+        ValidatorModel validation = roomValidator.exists(room);
+
+        if(!validation.isValid()) {
+            return new RoomServiceResponse<>(
+                RoomEnum.NOT_FOUND.getHttpStatus(),
+                RoomEnum.NOT_FOUND.getCode(),
+                RoomEnum.NOT_FOUND.getMessage(),
+                new RoomModel()
+            );
+        }
+
+        return new RoomServiceResponse<>(
+                RoomEnum.OK.getHttpStatus(),
+                RoomEnum.OK.getCode(),
+                RoomEnum.OK.getMessage(),
+                room.get()
+        );
     }
 
     public RoomServiceResponse<RoomModel> update(RoomModel updatedRoom) {
 
-        RoomServiceResponse<RoomModel> response = roomValidator
-                .exists(roomRepository.findById(updatedRoom.getId()));
+        Optional<RoomModel> room = roomRepository.findById(updatedRoom.getId());
 
+        ValidatorModel validation = roomValidator.exists(room);
 
-        if (!response.getCode().equals(RoomEnum.OK.getCode())) {
-            return response;
+        if (!validation.isValid()) {
+            return new RoomServiceResponse<>(
+                    RoomEnum.NOT_FOUND.getHttpStatus(),
+                    RoomEnum.NOT_FOUND.getCode(),
+                    RoomEnum.NOT_FOUND.getMessage(),
+                    new RoomModel()
+            );
         }
 
-        RoomModel payload = (RoomModel) response.getPayload();
-        payload.setSeats(updatedRoom.getSeats());
-
-        return response;
+        return new RoomServiceResponse<>(
+                RoomEnum.OK.getHttpStatus(),
+                RoomEnum.OK.getCode(),
+                RoomEnum.OK.getMessage(),
+                this.roomRepository.update(updatedRoom)
+        );
     }
 
     public RoomServiceResponse<RoomModel> create(RoomModel room) {
 
-        RoomServiceResponse<RoomModel> response = roomValidator
+        ValidatorModel response = roomValidator
                 .alreadyInList(roomRepository.findById(room.getId()));
 
-        if (!response.getCode().equals(RoomEnum.CREATED.getCode())) {
-            return response;
+        if (!response.isValid()) {
+            return new RoomServiceResponse<>(
+                    RoomEnum.ROOM_ALREADY_CREATED.getHttpStatus(),
+                    RoomEnum.ROOM_ALREADY_CREATED.getCode(),
+                    RoomEnum.ROOM_ALREADY_CREATED.getMessage(),
+                    new RoomModel()
+            );
         }
 
-        response.setPayload(this.roomRepository.create(room));
-
-        return response;
+        return new RoomServiceResponse<>(
+                RoomEnum.CREATED.getHttpStatus(),
+                RoomEnum.CREATED.getCode(),
+                RoomEnum.CREATED.getMessage(),
+                this.roomRepository.create(room)
+        );
     }
 
-    public RoomServiceResponse<RoomModel> delete(RoomModel room) {
-        RoomServiceResponse<RoomModel> response = roomValidator
-                .exists(roomRepository.findById(room.getId()));
+    public RoomServiceResponse<RoomModel> delete(RoomModel roomToDelete) {
+        Optional<RoomModel> room = roomRepository.findById(roomToDelete.getId());
+
+        ValidatorModel validation = roomValidator.exists(room);
 
 
-        if (!response.getCode().equals(RoomEnum.OK.getCode())) {
-            return response;
+        if (!validation.isValid()) {
+            return new RoomServiceResponse<>(
+                    RoomEnum.NOT_FOUND.getHttpStatus(),
+                    RoomEnum.NOT_FOUND.getCode(),
+                    RoomEnum.NOT_FOUND.getMessage(),
+                    new RoomModel()
+            );
         }
 
-        response.setPayload(this.roomRepository.delete(room));
-
-        return response;
+        return new RoomServiceResponse<>(
+                RoomEnum.OK.getHttpStatus(),
+                RoomEnum.OK.getCode(),
+                RoomEnum.OK.getMessage(),
+                this.roomRepository.delete(roomToDelete)
+        );
     }
 }
